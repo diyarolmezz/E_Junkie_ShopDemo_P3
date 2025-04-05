062010364ce3fc14da9963cd7156b0584443cdac
diyar1 <diyarolmezz124@gmail.com>
2025-04-05 10:11:48 -0700

    Duzenlemeler yapildi Test e hazir

---
+++ src/Utility/BaseDriver.java
@@ -0,0 +1,54 @@
+package Utility;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+
+public class BaseDriver {
+
+    // aşağısını SDET8 den aldık
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    @BeforeClass
+    public void Setup()
+    {
+        // seleniumdaki static yani başlangıç kısımlar konacak
+        driver=new ChromeDriver();
+
+        driver.manage().window().maximize(); // Ekranı max yapıyor.
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30)); // 20 sn mühlet: sayfayı yükleme mühlet
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); // 5 sn mühlet: elementi bulma mühleti
+
+        wait=new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        LoginTest();
+    }
+
+    public void LoginTest()
+    {
+
+        driver.get("https://shopdemo.fatfreeshop.com/?");
+        MyFunc.Bekle(2);
+
+
+    }
+
+
+    @AfterClass
+    public void TearDown()
+    {
+        // seleniumdaki BekleKapat
+        MyFunc.Bekle(3);
+        driver.quit();  // bütün açılmış windowları kapatır
+    }
+
+
+}
